73eb052d0f1c497c1f0917746938595db9141cf8
United-kx <mahiralt70@gmail.com>
2025-12-26 16:31:50 +0530

    fix3v6

--- api/index.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const { default: app } = await import('../src/server.js');
-  return app(req, res);
-}
